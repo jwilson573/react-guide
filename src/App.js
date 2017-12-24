0631e54d678f6db446b8888e0959e40401363502
Jonathon Wilson <jonathon.b.wilson@gmail.com>
2017-12-24 13:03:58 -0600

    Second Commit, prior to eject

--- src/App.js
+++ src/App.js
@@ -7,58 +7,115 @@ class App extends Component {
   //State only available in components that extends Component, not just a simple function component
   state = {
     persons: [
-      { name: 'Jon', age: 28 },
-      { name: 'Brad', age: 29 },
-      { name: 'Wilson', age: 30 }
+      { id: 1, name: 'Jon', age: 28 },
+      { id: 2, name: 'Brad', age: 29 },
+      { id: 3, name: 'Wilson', age: 30 }
     ],
-    otherState: "some other value"
+    otherState: "some other value",
+    showPersons: false
   }
-  switchNameHandler = (newName) => {
-    // console.log("Was Clicked");
-    // DON'T DO THIS: this.state.person[0].name = "Jonathon"
-    this.setState({persons: [
-        { name: newName, age: 28 },
-        { name: 'Bradley', age: 400 },
-        { name: 'Wilson', age: 32 }
-      ] 
-    })
+  // switchNameHandler = (newName) => {
+  //   // console.log("Was Clicked");
+  //   // DON'T DO THIS: this.state.person[0].name = "Jonathon"
+  //   this.setState({persons: [
+  //       { name: newName, age: 28 },
+  //       { name: 'Bradley', age: 400 },
+  //       { name: 'Wilson', age: 32 }
+  //     ] 
+  //   })
+  // }
+
+
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    // Alternative to the above code
+    // const persons = Object.assign({}, this.state.persons[personIndex])
+
+    person.name = event.target.value
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+
+    this.setState({persons: persons})
+  }
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons
+    this.setState({showPersons: !doesShow})
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({persons: [
-      { name: 'Jon', age: 28 },
-      { name: event.target.value, age: 400 },
-      { name: 'Wilson', age: 32 }
-    ] 
-  })
+  deletePersonHandler = (personIndex) => {
+    //const persons = this.state.persons.slice() <- alternative to the spread operator ...
+    const persons = [...this.state.persons]
+    persons.splice(personIndex, 1);
+    this.setState({persons: persons})
   }
 
   render() {
     const style = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
       font: 'inherit',
+      color: 'white',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      marginBottom: '20px',
+      cursor: 'pointer',
     };
+
+    let persons = null;
+
+    if(this.state.showPersons){
+      persons = (
+        <div>
+          {this.state.persons.map((person, idx) => {
+            return(
+              <Person 
+                name={person.name} 
+                age={person.age} click={() => this.deletePersonHandler(idx)} 
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event, person.id)}/>
+            )
+          })}          
+          {/* Use props.children to access JSX between custom elements */}
+        </div>
+      );
+      style.backgroundColor = 'red';
+    }
+    let classes = [];
+    let warning = null;
+
+    if(this.state.persons.length <= 2){
+      classes.push('red');
+      warning = "Running out of people to remove."
+    }
+    if(this.state.persons.length <= 1){
+      classes.push('bold');
+      warning = "Running out of people to remove.";
+    } 
+    if (this.state.persons.length === 0){
+      warning = "No more people to remove.";
+    }
+
     return (
-      <div className="App">
-        <h1>Hello, I'm a React App.</h1>
-        <button
-          style={style} 
-          onClick={this.switchNameHandler.bind(this, 'Jonathon!!')}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        {/* Use props.children to access JSX between custom elements */}
-        <Person 
-          name={this.state.persons[1].name} 
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Jon !!')}
-          changed={this.nameChangedHandler}> My Hobbies: Drumming 
-        </Person>
-        <Person 
-          name={this.state.persons[2].name} 
-          age={this.state.persons[2].age}/>
-      </div>
+     
+        <div className="App">
+          <h1>Hello, I'm a React App.</h1>
+          <p className={classes.join(' ')}>{warning}</p>
+          <button
+            style={style}
+            onClick={this.togglePersonHandler}> Show People
+          </button>
+          {persons}
+          
+        </div>
+     
     );
 
     //createElement takes at least 3 arguments, can have infinite number. Can use nested React.createElement. This is what JSX is compiled into
